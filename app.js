f7550a5bbda49ee4104acbe9a6dec3c7c4d66c67
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-11-11 17:40:19 -0300

    Configurando server iniciar

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("I am you Script")
+const http = require('http')
+
+const port = 3000
+
+const server = (req, res) => {
+    res.end("Soy tu Script...")
+}
+
+http.createServer( server ).listen(port)
+
+
+
+
+
+
+
+
+
+/* console.log("I am you Script") */
