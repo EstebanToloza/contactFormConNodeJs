1fd17601eeaa0d6247de57e7a4e498e9feffa79f
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-11-11 18:14:23 -0300

    Sirviendo archivos con FileSystem

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http')
+const fs = require('fs') //módulo para manera manejar el sistema de archivos
 
 const port = 3000
 
 const server = (req, res) => {
-    res.end("Soy tu Script...")
+
+    fs.readFile('front/index.html', (error, file) => {
+
+        if(error){
+            res.writeHead(404, { "Content-Type" : "text/plain"})
+            res.end(`Malio sal...`)
+        } else {
+            res.writeHead(200, { "Content-Type" : "text/html"})
+            res.end(file)
+        }
+    })
+    //res.end("Soy tu Script...")
 }
 
-http.createServer( server ).listen(port)
+http.createServer( server ).listen(port) //configuro las propiedades del servidor
 
 
 
