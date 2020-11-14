32f47dd8fbc581e72c5397795685a065c2744066
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-11-14 16:04:20 -0300

    Creando front de form de contacto

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express()
 const port = 3000 //configurando puerto
 
 app.listen(port) //se configura que escuche el puerto asignado
+app.use( express.static('public') ) //indicamos la ubicación del archivo de contacto en la carpeta 'public'
+
+
+
+
+
 /* 
 -Plantilla modelo para "endpoints" de express()
 app.TIPO_HTTP("/RUTA", (req, res) => {  
@@ -12,4 +18,8 @@ app.TIPO_HTTP("/RUTA", (req, res) => {
  */
 app.get("/contacto", (req, res) => {  
     res.end('Desde acá vamos a contactarnos...')
+})
+
+app.post("/enviar", (req, res) => {  
+    res.end('Desde acá enviamos..')
 })
