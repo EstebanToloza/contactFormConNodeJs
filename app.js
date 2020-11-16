14ae1effbe26fa7ad823c026814b32bd795b4f04
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-11-16 17:03:39 -0300

    Enviador de emails con Nodemailer

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const nodemailer = require("nodemailer")
 
 const port = 3000 //configurando puerto
+
+const miniOutlook = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'emanuel76@ethereal.email',
+        pass: 'GpPkBZsjYs4GQWjvKt'
+    },
+});
 
 app.listen(port) //se configura que escuche el puerto asignado
 app.use( express.static('public') ) //indicamos la ubicación del archivo de contacto en la carpeta 'public'
 app.use ( express.urlencoded({ extended : true }) ) //de x-www-urlencoded a objetos.
-
 
 /* 
 -Plantilla modelo para "endpoints" de express()
@@ -17,8 +26,15 @@ app.TIPO_HTTP("/RUTA", (req, res) => {
 
 app.post("/enviar", (req, res) => {  
     const contacto = req.body
-    console.log(contacto)
-    res.end('Desde acá enviamos..')
+    
+    miniOutlook.sendMail({
+        from : contacto.correo, // sender address
+        to : "########@mail.com", // list of receivers
+        subject : `Asunto #${contacto.asunto}`, // Subject line
+        text : "Hello world?", // plain text body
+        html : `<blockquote>${contacto.mensaje}</blockquote>`, // html body
+      });
+    res.end('Desde acá vamos a enviar un email de contacto...')
 })
 
 
